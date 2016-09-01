10fcf9c0070a5d7034ab1ce3d316e5b2ddb169b2
root <hawk.alan@gmail.com>
2016-09-01 06:04:14 +0000

    split into seperate file

--- Text/XMLParser/Util.hs
+++ Text/XMLParser/Util.hs
@@ -1,11 +1,11 @@
 module Text.XMLParser.Util (
-
  getTagOfNameChildren
 ,getTagsByName
 ,getNonEmptyTagOfNameChildren
 ,getNonEmptyTagsByName
-
 ) where 
+
+import Text.XMLParser.XMLParser
 
 
 makeNonEmpty [] = Nothing
@@ -13,8 +13,8 @@ makeNonEmpty xs@(_:_) = Just xs
 
 getTagName (Tag _ str _) = str  
 
-getTagOfNameChildren str (Tag _ str’ (Just xs)) 
-  | str == str’ = xs
+getTagOfNameChildren str (Tag _ str (Just xs)) 
+  | str == str = xs
   | otherwise = Nothing
 
 getTagsByName str xs = filter ((==str) . getTagName) xs
