f7efd1887660bfa2ff37dddd37fb239f230936db
Alan Hawkins <alanpika_au@yahoo.com>
2016-09-01 15:35:36 +1000

    Create Util.hs

---
+++ Text/XMLParser/Util.hs
@@ -0,0 +1,23 @@
+module Text.XMLParser.Util (
+
+ getTagOfNameChildren
+,getTagsByName
+,getNonEmptyTagOfNameChildren
+,getNonEmptyTagsByName
+
+) where 
+
+
+makeNonEmpty [] = Nothing
+makeNonEmpty xs@(_:_) = Just xs
+
+getTagName (Tag _ str _) = str  
+
+getTagOfNameChildren str (Tag _ str’ (Just xs)) 
+  | str == str’ = xs
+  | otherwise = Nothing
+
+getTagsByName str xs = filter ((==str) . getTagName) xs
+
+getNonEmptyTagOfNameChildren str tag = getTagOfNameChildren >>= makeNonEmpty
+getNonEmptyTagsByName str xs = getTagsByName >>= makeNonEmpty
