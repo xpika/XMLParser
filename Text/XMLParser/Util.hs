18457a61c78c74ffdd6b48d3d885038277842f1f
Alan Hawkins <alanpika_au@yahoo.com>
2016-09-03 15:54:37 +1000

    Update Util.hs

--- Text/XMLParser/Util.hs
+++ Text/XMLParser/Util.hs
@@ -4,6 +4,7 @@ module Text.XMLParser.Util (
 ,getTagsByName
 ,getNonEmptyTagOfNameChildren
 ,getNonEmptyTagsByName
+,getNonEmptyAttributeByName
 ) where 
 
 import Text.XMLParser.XMLParser
@@ -11,13 +12,22 @@ import Text.XMLParser.XMLParser
 makeNonEmpty [] = Nothing
 makeNonEmpty xs@(_:_) = Just xs
 
-getTagName (Tag _ str _) = str  
+getTagName (Tag _ str _) = Just str
+getTagName _ = Nothing
+
+getTagText (TagString a) = Just a
+getTagText _ = Nothing
+
+getTagChildText (Tag _ _ xs) = catMaybes . map getTagText xs
 
 getTagOfNameChildren str (Tag _ str' (Just xs)) 
   | str == str' = Just xs
   | otherwise = Nothing
 
-getTagsByName str xs = filter ((==str) . getTagName) xs
+getTagsByName str xs = filter (maybe False (==str) . getTagName) xs
 
 getNonEmptyTagOfNameChildren str tag = getTagOfNameChildren str tag >>= makeNonEmpty
 getNonEmptyTagsByName name xs = makeNonEmpty $ getTagsByName name xs 
+getNonEmptyAttributeByName attr (Tag attrs _ _) = lookup attr attrs 
+getNonEmptyAttributeByName attr (Tag attrs _ _) = lookup attr attrs 
+
